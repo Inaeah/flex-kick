74e1e282d0040bc81b60cbe8bf1bb3f82233b933
Inaeah <nayahsaffanah@gmail.com>
2025-09-08 21:36:44 +0700

    Add MVT implementation

---
+++ main/models.py
@@ -0,0 +1,24 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('clothing', 'Clothing'),
+        ('footwear', 'Footwear'),
+        ('bag', 'Bag'),
+        ('equipment', 'Equipment'),
+        ('merchandise', 'Merchandise'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=40)
+    price = models.IntegerField()
+    brand = models.CharField(max_length=50)
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
